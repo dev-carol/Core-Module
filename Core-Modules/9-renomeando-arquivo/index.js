4259a7cfdd2d6d43e7bfb6b20013968781ce0f4d
carol santos <contato.caroline1999@gmail.com>
2022-06-14 14:54:24 -0300

    lesson: rename files

---
+++ Core-Modules/9-renomeando-arquivo/index.js
@@ -0,0 +1,14 @@
+const fs = require ('fs')
+
+const arquivoAntigo = "arquivo.txt"
+const arquivoNovo = "novo.txt"
+
+fs.rename(arquivoAntigo, arquivoNovo, function(err){
+
+    if(err){
+        console.log(err);
+        return 
+    }
+
+    console.log(`O arquivo ${arquivoAntigo} foi renomeado para ${arquivoNovo}`);
+})
