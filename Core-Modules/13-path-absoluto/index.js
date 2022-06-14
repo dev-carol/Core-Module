b7bca2c6695f37c546ae8c2fe7c4cd53da37b8a9
carol santos <contato.caroline1999@gmail.com>
2022-06-14 16:08:31 -0300

    lesson: path absolute

---
+++ Core-Modules/13-path-absoluto/index.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+
+//path absoluto
+
+console.log(path.resolve('teste.txt'));
+
+// formar path
+
+
+const midFolder = "relatorios"
+const fileName = "carol.txt"
+
+const finalPath = path.join("/",'arquivos', midFolder, fileName)
+
+console.log(finalPath);
